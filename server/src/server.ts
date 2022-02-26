1e879018c94ef92099a31762477f2f7097e6bb7f
Robert Thornton <robert@pebble-tree.co.uk>
2022-02-26 11:49:35 +0100

    tidy up

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express"
-
+import * as path from "path"
 const app = express()
 app.listen(1369,()=>{
     console.log("Game server started");
 })
-app.use(express.static(__dirname + "/../../client/public/"))
+app.use(express.static( path.join( __dirname ,"..","..","client","public")))
